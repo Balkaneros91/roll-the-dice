f4d4edb99736ae80b2a10a67d1a12f6642aa839e
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2022-11-23 14:43:43 +0000

    Add event listener to DOM and buttons

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+/**
+ * Loading the DOM first before any other game action
+ * looping through buttons and add event listener
+ */
+ document.addEventListener('DOMContentLoaded', function () {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function (event) {
+            if (this.getAttribute('data-type') === 'submit') {
+                rollDice();
+            } else if (this.getAttribute('data-type') === 'reset') {
+                window.location.reload(true);
+            } else {
+                let targNr = this.getAttribute('data-choice');
+                document.getElementById("targ-Nr").innerHTML = targNr;
+                document.getElementById("roll").disabled = false;
+            }
+        });
+    }
+});
